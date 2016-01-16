93143a46b87209d578baadfe43c079474a81df32
Nukui <nukui.s.aa@gmail.com>
2016-01-16 14:55:12 +0900

    Extend for cross_validation in sklearn

--- tfranknet/ranknet.py
+++ tfranknet/ranknet.py
@@ -11,12 +11,13 @@ from __future__ import absolute_import
 
 from six.moves import xrange
 import numpy as np
+from sklearn.base import BaseEstimator
 import tensorflow as tf
 
 ACTIVATE_FUNC = {"relu": tf.nn.relu,
                  "sigmoid": tf.nn.sigmoid}
 
-class RankNet(object):
+class RankNet(BaseEstimator):
     """Class for RankNet
 
         References
@@ -29,14 +30,15 @@ class RankNet(object):
     """
 
     def __init__(self, hidden_units, batch_size=32, activate_func="relu",
-                 learning_rate=0.01, max_steps=1000, verbose=False,
+                 learning_rate=0.01, max_steps=1000, sigma=1.0,
                  logdir=None, q_capacity=1000000, min_after_dequeue=100,
-                 threads=8):
+                 threads=4, verbose=False):
         if not activate_func in ACTIVATE_FUNC:
             raise ValueError("'activate_func' must be in"
                             "['rele', 'sigmoid'")
         self.activate_func = activate_func
         self.hidden_units = hidden_units
+        self.sigma = sigma
         self.learning_rate = learning_rate
         self.q_capacity = q_capacity
         self.max_steps = max_steps
@@ -46,20 +48,24 @@ class RankNet(object):
         self.batch_size = batch_size
         self.logdir = logdir
 
-    def fit(self, data1, data2, pretraining=False, init=True):
-        """Learn the neural network
-            The ranks of data1 are labeled higher than data2
+    def fit(self, data, pretraining=False, init=True, label=None):
+        """Learn the ranking neural network
+            The ranks of data1[i] are labeled higher than data2[i]
+
+            The input data must be packed by pack_data(data1, data2)
+                before calling this function
 
             Arguments
             ---------------
-            data1 : 2-D numpy array or Pandas DataFrame
-                    which are higher rank than data2
-            data2 : Must have the same shape as data1
+            datas : A list of 2-D numpy array or Pandas DataFrame
+                    such that datas[0][i] are higher rank than data[1][i]
+            pretraining : If True pretrain by AutoEncoder(Not implemented)
+            init : If True initialize the graph and variables
+            label : A dummy argument for cross validation in skleran
         """
+        data1, data2 = self.unpack_data(data)
         shape1 = tuple(data1.shape)
         shape2 = tuple(data2.shape)
-        if shape1 != shape2:
-            raise ValueError("'data1' and 'data2' mush have the same shape")
 
         self.fdim = shape1[1]
         self.data_size = shape1[0]
@@ -99,22 +105,30 @@ class RankNet(object):
         coord.join(enqueue_threads)
         #prepare prediction
 
-    def predict(self, data1, data2):
+    def predict(self, data):
+        """Predict whether data1[i] is higher rank than data2[i]
         """
-        Predict whether data1[i] is higher rank than data2[i]
-        """
-        prob = self.predict_prob(data1, data2)
-        pred = prob > 0.5
+        prob = self.predict_prob(data)
+        pred = (prob >= 0.5)
         return pred
 
-    def predict_prob(self, data1, data2):
+    def predict_prob(self, data):
         """
-        Predict probability that data1[i] is higher rank than data2[i]
+        Predict probabilities that data1[i] is higher rank than data2[i]
         """
+        data1, data2 = self.unpack_data(data)
         sess = self.sess
         feed_dict = {self.input1: data1, self.input2: data2}
         prob = sess.run(self.prob, feed_dict=feed_dict)
         return prob
+
+    def get_scores(self, data):
+        """
+        Compute scores for a unpacked data
+        """
+        feed_dict = {self.input1: data}
+        scores = self.sess.run(self.score, feed_dict=feed_dict)
+        return scores
 
     def _setup_training(self):
         self.graph = tf.Graph()
@@ -125,6 +139,7 @@ class RankNet(object):
             layer_units = [fdim] + hidden_units + [1]
             layer_num = len(layer_units)
             act_func = ACTIVATE_FUNC[self.activate_func]
+            sigma = self.sigma
             lr = self.learning_rate
 
             #make Queue for getting batch
@@ -152,8 +167,8 @@ class RankNet(object):
 
                 with tf.name_scope("cost"):
                     self.cost = cost = tf.reduce_sum(
-                                        tf.log(1 + tf.exp(-tf.nn.sigmoid(s1-s2))))
-
+                                        tf.log(1 + tf.exp(-sigma*(s1-s2))))
+            self.score = s1
             optimizer = tf.train.GradientDescentOptimizer(lr)
             self.optimize = optimizer.minimize(cost)
 
@@ -177,11 +192,12 @@ class RankNet(object):
             weights = self.weights
             biases = self.biases
             act_func = ACTIVATE_FUNC[self.activate_func]
+            sigma = self.sigma
             with tf.name_scope("prediction"):
                 s1 = self._obtain_score(inp1, weights, biases, act_func, "1")
                 s2 = self._obtain_score(inp2, weights, biases, act_func, "2")
                 with tf.name_scope("probability"):
-                    self.prob = 1 / (1 + tf.exp(-tf.nn.sigmoid(s1-s2)))
+                    self.prob = 1 / (1 + tf.exp(-sigma*(s1-s2)))
 
     @classmethod
     def _obtain_score(cls, data, weights, biases, act_func, label):
@@ -213,10 +229,47 @@ class RankNet(object):
         b = tf.Variable(init, name=name)
         return b
 
-    def _set_weight(weight, layer):
+
+    def _set_weight(self, weight, layer):
         assign_op = self.weights[layer].assign(weight)
         self.sess.run(assign_op)
 
-    def _set_bias(bias, layer):
+    def _set_bias(self, bias, layer):
         assign_op = self.biases[layer].assign(bias)
         self.sess.run(assign_op)
+
+    @staticmethod
+    def unpack_data(data):
+        """
+        Unpack a packed data
+        """
+        e = TypeError("'data' must be packed by pack_data() at the beginning")
+        try:
+            shape = data.shape
+        except Exception:
+            raise e
+        if (len(shape) != 3) or (shape[1] != 2):
+            raise e
+        data1 = data[:,0,:]
+        data2 = data[:,1,:]
+        return data1, data2
+
+    @staticmethod
+    def pack_data(data1, data2):
+        """Pack two data for preprocessing
+            By this function cross_validation() in sklearn can be applied
+            for this class
+        """
+        e = TypeError("'data1' and 'data2' must be 2-D numpy array or"
+                    "Pandas DataFrame such that data1.shape == data2.shape")
+        try:
+            shape1 = tuple(data1.shape)
+            shape2 = tuple(data2.shape)
+        except Exception:
+            raise e
+        if (shape1 != shape2):
+            raise e
+        data1 = np.array(data1)
+        data2 = np.array(data2)
+        packed = np.array([data1, data2]).swapaxes(0,1)
+        return packed
--- tfranknet/ranknet_test.py
+++ tfranknet/ranknet_test.py
@@ -1,14 +1,25 @@
 import os
 import numpy as np
 from ranknet import RankNet
+from sklearn.cross_validation import cross_val_predict
 
 os.system("rm -rf testlog")
 data1 = np.random.rand(10000, 30)
 data2 = np.random.rand(10000, 30)
+label = [True]*10000
 
 rn = RankNet(hidden_units=[20, 10], logdir="testlog",
              learning_rate=0.01)
-rn.fit(data1, data2)
+data = rn.pack_data(data1, data2)
 
-prob = rn.predict_prob(data1, data2)
-pred = rn.predict(data1, data2)
+cvpred = cross_val_predict(rn, data, label, cv=2)
+
+rn.fit(data)
+
+input1 = np.random.rand(10, 30)
+input2 = np.random.rand(10, 30)
+input_ = rn.pack_data(input1, input2)
+prob = rn.predict_prob(input_)
+pred = rn.predict(input_)
+score = rn.get_scores(input1)
+score = rn.get_scores(input2)
